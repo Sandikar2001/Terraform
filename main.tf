dde8cffbdeaabc0c543f4f5917265ca48b5d03ce
Sandikar2001 <sandikarrushikesh2001@gmail.com>
2025-06-26 12:32:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,23 +14,24 @@ module "ec2" {
   env         = var.environment
   ec2_instances = {
     for name, inst in var.ec2_instance_base :
-    name => {
-      ami           = inst.ami
-      instance_type = inst.instance_type
-      key_name      = inst.key_name
-      subnet_id = (
-        inst.subnet_group == "public" ? module.vpc.public_subnet_ids[inst.subnet_key] :
-        inst.subnet_group == "app" ? module.vpc.app_subnet_ids[inst.subnet_key] :
-        inst.subnet_group == "db" ? module.vpc.db_subnet_ids[inst.subnet_key] :
-        null
-      )
-      tags       = inst.tags
-      assign_eip = inst.assign_eip
-    }
+    name => merge(
+      inst, 
+      {
+        subnet_id = (
+          inst.subnet_group == "public" ? module.vpc.public_subnet_ids[inst.subnet_key] :
+          inst.subnet_group == "app"    ? module.vpc.app_subnet_ids[inst.subnet_key] :
+          inst.subnet_group == "db"     ? module.vpc.db_subnet_ids[inst.subnet_key] :
+          null
+        )
+      }
+    )
   }
+  
   sg_egress         = var.sg_egress
   sg_ingress        = var.sg_ingress
   vpc_id            = module.vpc.vpc_id
   public_subnet_ids = values(module.vpc.public_subnet_ids)
+
+  depends_on = [module.vpc]
 }
 
