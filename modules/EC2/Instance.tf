d12aaf9f580450a2393044e9d0efcb04ca88b710
Sandikar2001 <sandikarrushikesh2001@gmail.com>
2025-06-30 10:58:29 +0530

    Update Instance.tf

--- modules/EC2/Instance.tf
+++ modules/EC2/Instance.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "ec2" {
   instance_type          = each.value.instance_type
   subnet_id              = each.value.subnet_id
   key_name               = each.value.key_name
-  associate_public_ip_address = false
+  associate_public_ip_address = try(each.value.associate_public_ip_address, false)
   vpc_security_group_ids = [aws_security_group.ec2_sg[each.key].id]
 
   tags = merge(
