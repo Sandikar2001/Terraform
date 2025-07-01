a5d291b0a68ebce70552ac5588c2041cbe984747
Sandikar2001 <sandikarrushikesh2001@gmail.com>
2025-07-01 12:30:33 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,12 +2,7 @@ output "ec2_instance_details" {
   value = module.ec2.ec2_instance_details
 }
 
-output "DEBUG_final_filtered_map" {
-  description = "Shows the content of the map used by the for_each. Should NOT be empty."
-  value       = local.instances_requiring_db_access
-}
-
-output "DEBUG_source_data_map" {
-  description = "Shows the full processed map BEFORE filtering. Check here for the flag."
-  value       = local.processed_instances
+output "LB_DNS_NAME" {
+  description = "The public DNS names for all Application Load Balancers."
+  value       = module.ec2.alb_dns_names
 }
