6e0c85b02d0759562b74c08ce5abc8f82a946a2f
Sandikar2001 <sandikarrushikesh2001@gmail.com>
2025-07-04 14:54:05 +0530

    Update Instance.tf

--- modules/EC2/Instance.tf
+++ modules/EC2/Instance.tf
@@ -8,34 +8,31 @@ resource "aws_instance" "ec2" {
   associate_public_ip_address = try(each.value.associate_public_ip_address, false)
   vpc_security_group_ids = [aws_security_group.ec2_sg[each.key].id]
   dynamic "root_block_device" {
-    # This loop runs ONCE if the root_block_device object exists for the instance.
-    for_each = try(each.value.root_block_device, null) != null ? [1] : []
+    # This for_each is a bit complex, let's simplify it.
+    # We'll create a list containing our object if it exists, or an empty list if it doesn't.
+    for_each = try([each.value.root_block_device], [])
 
     content {
-      # We reference the attributes from the object in our tfvars.
-      volume_size = each.value.root_block_device.volume_size
-      volume_type = each.value.root_block_device.volume_type
-      # You can also add iops, throughput, etc. here if using io1/io2/gp3
+      # Use the dynamic block's iterator: `root_block_device.value`
+      volume_size = root_block_device.value.volume_size
+      volume_type = root_block_device.value.volume_type
     }
   }
 
-  # --- NEW: Dynamic EBS Block Devices for Data ---
+  # --- CORRECTED EBS Block Devices ---
   dynamic "ebs_block_device" {
-    # This loops over the MAP of ebs_block_devices we defined.
-    # The iterator will be 'ebs_block_device'.
     for_each = try(each.value.ebs_block_devices, {})
 
     content {
-      # ebs_block_device.key is the device name (e.g., "/dev/sdf")
+      # Use the dynamic block's iterator: `ebs_block_device`
+      # `ebs_block_device.key` is the device name (e.g., "/dev/sdf")
       device_name = ebs_block_device.key
 
-      # ebs_block_device.value contains the configuration object
+      # `ebs_block_device.value` is the configuration object
       volume_size = ebs_block_device.value.volume_size
       volume_type = ebs_block_device.value.volume_type
       tags        = try(ebs_block_device.value.tags, {})
-      
-      # Important for data volumes: ensure they are deleted when the instance is terminated.
-      delete_on_termination = true 
+      delete_on_termination = true
     }
   }
 
