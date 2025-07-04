fb04b13a7744f11dee3229613ff8d25fc417d88d
Sandikar2001 <sandikarrushikesh2001@gmail.com>
2025-07-04 15:27:05 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -6,3 +6,8 @@ output "LB_DNS_NAME" {
   description = "The public DNS names for all Application Load Balancers."
   value       = module.ec2.alb_dns_names
 }
+
+output "DEBUG_PROCESSED_INSTANCES_FINAL_CHECK" {
+  description = "Shows the final data map being passed to the EC2 module."
+  value       = local.processed_instances
+}
